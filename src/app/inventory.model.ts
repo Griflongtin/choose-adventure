9d8547600cc6b4ac9147a30ad40a4575cf8a108d
Cheryl Frankenfield and Griffin P Longtin <griffin@>
2018-01-24 11:53:05 -0800

    Add sample inventory json file

--- src/app/inventory.model.ts
+++ src/app/inventory.model.ts
@@ -1,18 +1,18 @@
 export class InventoryFood {
-  constructor(public item: string, public healthy: number, public hazard: number, public weight: number){}
+  constructor(public item: string, public healthy: number, public hazard: number, public weight: number, public damage: number, public worth: number){}
 }
 export class InventoryMedic{
-  constructor(public item: string, public heals: number, public weight: number){}
+  constructor(public item: string, public heals: number, public weight: number, public damage: number, public worth: number){}
 }
 export class InventoryTools{
-  constructor(public item: string, ){}
+  constructor(public item: string, public weight: number, public damage: number, public worth: number){}
 }
-export class InventoryBitCoins{
-  constructor(public item: string, ){}
+export class InventoryCoins{
+  constructor(public item: string, public worth: number){}
 }
 export class InventoryWeapons{
-  constructor(public item: string, ){}
+  constructor(public item: string, public attack: number, public weight: number, public damage: number, public ammo: number, public worth: number ){}
 }
 export class InventoryAmmo{
-  constructor(public item: string, ){}
+  constructor(public item: string, public weight: number, public armorPiercing: number, public worth: number){}
 }
