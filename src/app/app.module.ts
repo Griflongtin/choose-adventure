7ce555776ac9725ba259e8e9ce537286363beb20
Cheryl Frankenfield and Griffin P Longtin <Guest@Epicodus-3E.local>
2018-01-23 16:21:42 -0800

    start of firebace api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
-
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MapComponent } from './map/map.component';
 import { PlaceComponent } from './place/place.component';
-import { InventoryService } from './inventory.service';
+import { UserService } from './user.service';
+import { masterFirebaseConfig } from './api-keys';
 
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
@@ -21,9 +30,12 @@ import { InventoryService } from './inventory.service';
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    HttpModule,
+    routing,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
-  providers: [InventoryService],
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from '../user.model';
+import { FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,11 @@ import { User } from '../user.model';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  users: FirebaseListObservable<any[]>;
   constructor(private router: Router) { }
 
   ngOnInit() {
+    console.log(this.users)
   }
   // goToMap() {
   //   alert('help');
